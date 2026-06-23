b7ae275146f5b4cf220e46d457184783651f3ff3
heyjyu <jiwoosmile@yonsei.ac.kr>
2026-06-24 00:14:04 +0900

    가민 D2.3 이미지 11장 추출(xlsx→images) + 가민이미지/레드미(색상수·스토어링크) 채움 SQL

---
+++ supabase_garmin_images.sql
@@ -0,0 +1,12 @@
+-- 가민 D2.3 이미지 채우기 (xlsx 추출 → images/ 푸시 후 RUN)
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_1.jpg' where name='가민 D2.3 호환 로드 솔리드 버클 실리콘 스트랩';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_2.jpg' where name='가민 D2.3 호환 호라이즌 버클 실리콘 스트랩';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_3.jpg' where name='가민 D2.3 호환 매시브 메탈 버클 실리콘 스트랩';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_4.jpg' where name='가민 D2.3 호환 투톤 블레이즈 마그넷 링크 버튼 실리콘 스트랩';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_5.jpg' where name='가민 D2.3 호환 볼트 블랙 버클 실리콘 스트랩';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_6.jpg' where name='가민 D2.3 호환 퀵핏 스트라이프 마그넷 링크 실리콘 스트랩';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_7.jpg' where name='가민 D2.3 호환 퀵핏 투톤 메카닉 블랙 버클 실리콘 스트랩';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_8.jpg' where name='가민 D2.3 호환 퀵핏 에센셜 솔리드 버클 실리콘 스트랩 26mm';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_9.jpg' where name='가민 D2.3 호환 러너 스포츠 실리콘 스트랩';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_10.jpg' where name='가민 D2.3 호환 러너 트랙 실리콘 스트랩';
+update public.products set image='https://heyjyu.github.io/spaceshied-catalog/images/19_11.jpg' where name='가민 D2.3 호환 스탠다드 울트라핏 모노 듀얼 밸크로 패브릭 스트랩';
---
+++ supabase_redmi_fill.sql
@@ -0,0 +1,15 @@
+-- 레드미 색상수(SKU)+네이버 스토어링크 채우기
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11394945841', color_count=3 where name='레드미 워치 5 호환 알파인 루프 스트랩';
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11394937941', color_count=3 where name='레드미 워치 5 호환 트레일 루프 스트랩';
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11394958513', color_count=4 where name='레드미 워치 5 호환 풀탭 스포츠루프 스트랩';
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11394983705', color_count=6 where name='레드미 워치 5 호환 마그넷 브레이드 스트랩';
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11394994877', color_count=6 where name='레드미 워치 5 호환 버클형 브레이드 스트랩';
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11394856433', color_count=10 where name='레드미 워치 5 호환 버튼형 베이직 실리콘 스트랩';
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11394866837', color_count=13 where name='레드미 워치 5 호환 버튼형 실리콘 스트랩';
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11394877943', color_count=10 where name='레드미 워치 5 호환 버튼형 스포티 실리콘 스트랩';
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11395000725', color_count=4 where name='레드미 워치 5 호환 스틸 스트랩';
+update public.products set store_url='https://smartstore.naver.com/spaceshied/products/11395010670', color_count=5 where name='레드미 워치 5 호환 메탈 스트랩';
+update public.products set color_count=12 where name='레드미 워치 5 호환 소프트 마그넷 링크 실리콘 스트랩';
+update public.products set color_count=6 where name='레드미 워치 5 호환 패브릭 레일 스포츠루프 나일론 스트랩';
+update public.products set color_count=7 where name='레드미 워치 5 호환 밀키 메쉬 메탈 스트랩';
+update public.products set color_count=15 where name='레드미 워치 5 호환 에어 슬로프 밸크로 패브릭 나일론 스트랩';
